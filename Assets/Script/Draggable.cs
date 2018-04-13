c228d8a1eeab0f863d81cddd480a4202fb922692
zifanshi <zifanshi@usc.edu>
2018-04-13 01:08:19 -0700

    card deck

---
+++ Assets/Script/Draggable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Transform cardDeck = null;
+
+	public void OnBeginDrag(PointerEventData pointerEventData) {
+		cardDeck = this.transform.parent;
+		this.transform.SetParent (this.transform.parent.parent);
+		Debug.Log (this.transform.parent.name);
+
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData pointerEventData) {
+		Debug.Log ("On drag" + this.transform.parent.name);
+		this.transform.position = pointerEventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData pointerEventData) {
+		this.transform.SetParent (cardDeck);
+		Debug.Log ("end drag " + this.transform.parent.name);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+}
---
+++ Assets/Script/DropZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler {
+
+	public void OnDrop(PointerEventData pointEventData) {
+
+		Draggable d = pointEventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null) {
+			Destroy (d.gameObject);
+		}
+	}
+}
