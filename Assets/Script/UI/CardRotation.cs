3303b1bdfbb6d075c7d68887e382256fc47ce56d
zifanshi <zifanshi@usc.edu>
2018-04-14 16:09:15 -0700

    card prefab done

---
+++ Assets/Script/UI/CardRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CardRotation : MonoBehaviour 
+{
+	[SerializeField] public RectTransform _cardFront;
+	[SerializeField] public RectTransform _cardBack;
+	[SerializeField] public Transform _cardRectTransform;
+
+	private bool _showingBack = false;
+
+	void Update ()
+	{
+		var angle = Mathf.Abs(_cardRectTransform.localEulerAngles.y);
+
+		if (!_showingBack && angle > 90f && angle < 270f)
+		{
+			_showingBack = true;
+			_cardFront.gameObject.SetActive(false);
+			_cardBack.gameObject.SetActive(true);
+		}
+		else if (_showingBack && angle < 90f || angle > 270f)
+		{
+			_showingBack = false;
+			_cardFront.gameObject.SetActive(true);
+			_cardBack.gameObject.SetActive(false);
+		}
+	}
+}
+
